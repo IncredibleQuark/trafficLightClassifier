7568d4815f6edfaa3b71d0eab411e3914f466294
IncredibleQuark <lkupinski@gmail.com>
2021-10-12 23:07:30 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2 as cv2
+import helpers
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# %matplotlib inline
+
+
+def main():
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main()
